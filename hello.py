67aa9859397e9f467c609fc561a3d5a78181b2a2
unknown <scott.neaves@SNEAVES81CHI.pariveda.local>
2014-12-03 11:10:15 -0600

    k'

--- hello.py
+++ hello.py
@@ -1,14 +1,11 @@
 from flask import Flask, request, render_template
-import os
 
 app = Flask(__name__)
 app.debug = True
-app.secret_key=os.urandom(24) #for session management
 
 @app.route("/", methods=['GET', 'POST'])
 def index():
-	return render_template("index.html")
-
+	return render_template("html.html")
 
 if __name__ == "__main__":
 	app.run(host='0.0.0.0')
